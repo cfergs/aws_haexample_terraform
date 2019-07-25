80f44cf1ace899c0eb52dfe2e4a890bb924810b3
Colin Fergusson <domainadmin@chickenwaffles.net>
2019-07-25 17:13:46 +1200

    add listener for lb

--- lb.tf
+++ lb.tf
@@ -17,3 +17,13 @@ resource "aws_lb_target_group" "frontend" {
     healthy_threshold = 2
   }
 }
+
+resource "aws_lb_listener" "lb_listener" {
+  load_balancer_arn = "${aws_lb.lb.arn}"
+  port              = "80"
+  protocol          = "HTTP"
+  default_action {
+    type  = "forward"
+    target_group_arn = "${aws_lb_target_group.frontend.arn}"
+  }
+}
